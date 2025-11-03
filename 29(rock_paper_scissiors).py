80d5c1de28f480a158080041688b61d027d461f6
Ssara Gupta <0212ssara@gmail.com>
2025-11-03 16:41:31 +0530

    updates

--- 29(rock_paper_scissiors).py
+++ 29(rock_paper_scissiors).py
@@ -2,22 +2,31 @@ import random
 
 print("🪨📃✂️ LET'S PLAY THE GAME 🪨📃✂️")
 options = ("rock", "paper", "scissors")
-player = None
-computer = random.choice(options)
 
-while player not in options:
-    player = input("Enter a choice (rock, paper, scissors): ")
+running = True
 
-print(f"Player: {player}")
-print(f"Computer: {computer}")
+while running:
+    player = None
+    computer = random.choice(options)
 
-if player == computer:
-    print("It's a tie🙀")
-elif player == "rock" and computer == "scissors":
-    print("You win!!🥳")
-elif player == "paper" and computer == "rock":
-    print("You win!!🥳")
-elif player == "scissors" and computer == "paper":
-    print("You win!!🥳")
-else:
-    print("You lose!😭")
+    while player not in options:
+        player = input("Enter a choice (rock, paper, scissors): ")
+
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("It's a tie🙀")
+    elif player == "rock" and computer == "scissors":
+        print("You win!!🥳")
+    elif player == "paper" and computer == "rock":
+        print("You win!!🥳")
+    elif player == "scissors" and computer == "paper":
+        print("You win!!🥳")
+    else:
+        print("You lose!😭")
+
+    if not input("Play again? (y for yes): ").lower() == "y":
+        running = False
+
+print("Thanks for playing!!🥳🥳")
